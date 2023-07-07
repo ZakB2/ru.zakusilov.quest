1397bab7325466d09cdef57f2d02509abacc9502
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-07-07 09:42:17 +0300

    The fields modifier was changed for use in the Test class

--- src/main/java/ru/zakusilov/quest/resolvers/ResultResolver.java
+++ src/main/java/ru/zakusilov/quest/resolvers/ResultResolver.java
@@ -1,8 +1,8 @@
 package ru.zakusilov.quest.resolvers;
 
 public class ResultResolver {
-    private static final String WIN = "Ура, капитан %s, ты выиграл!";
-    private static final String LOSE = "Увы, капитан %s, ты проиграл...";
+    public static final String WIN = "Ура, капитан %s, ты выиграл!";
+    public static final String LOSE = "Увы, капитан %s, ты проиграл...";
 
     public String getResult(String name, Boolean answer) {
         return Boolean.TRUE.equals(answer) ? String.format(WIN, name) : String.format(LOSE, name);
